73754331a31ee8568ad514017f83d866df6119e7
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-01-17 22:44:52 -0600

    Added 2 Function Inside Of threshhold_image; Added 2 More Functions Outside Of threshhold_image

--- Simple_Point_Operations_with_OpenCV/Threshholding.py
+++ Simple_Point_Operations_with_OpenCV/Threshholding.py
@@ -6,12 +6,45 @@ import cv2
 import numpy as np
 
 # Converts To A Threshhold Image Using Formula
-def threshhold_image(image):
-    threshhold = np.where(image > 128,255,0)
-    threshhold = threshhold.clip(0,255).astype(np.uint8)
+def threshhold_image(image,looping):
+    # Converts Full IMage Using Formula
+    def formula():
+        threshhold = np.where(image > 128,255,0)
+        threshhold = threshhold.clip(0,255).astype(np.uint8)
+
+        return threshhold
     
+    # Loops Through Each Pixel And Converts Based On Formula
+    def looping_formula():
+        rows, cols = image.shape
+        threshhold = image.copy()
+        
+        for r in range(rows):
+            for c in range(cols):
+                threshhold[r][c]= 255 if image[r][c] > 128 else 0
+
+        return threshhold
+
+    threshhold = looping_formula() if looping else formula()
+
     return threshhold
 
-# Converts To A Threshhold Image Using LUT(look up table)
-def threshhold_image_LUT(img):
-    print()
+# Threshhold LUT Table
+def create_lut_threshhold():
+    lut=[]
+    for i in range(256):
+        lut.append(255 if (i>128) else 0)
+    return lut
+
+# Converts Image To Threshhold Using LUT(look up table)
+def threshhold_image_LUT(image):
+    lut_threshhold  = create_lut_threshhold()
+    rows, cols = image.shape
+    threshhold_LUT = image.copy()
+    
+    # Iterates Through Image Pixels And Converts Image Based On LUT
+    for r in range(rows):
+        for c in range(cols):
+            threshhold_LUT[r][c]=lut_threshhold[image[r][c]]
+
+    return threshhold_LUT
