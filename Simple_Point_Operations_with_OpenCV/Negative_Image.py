24f472eab09f2d6e7c575f01dcacafc95e792e12
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-01-17 22:43:56 -0600

    Added 2 Functions To negative_image; Created 2 More Functions Outside Of negative_image

--- Simple_Point_Operations_with_OpenCV/Negative_Image.py
+++ Simple_Point_Operations_with_OpenCV/Negative_Image.py
@@ -4,12 +4,46 @@ import cv2
 import numpy as np
 
 # Converts To A Negative Image Using Formula
-def negative_image(image):
-    negative = 255-image
-    negative = negative.clip(0,255).astype(np.uint8)
+def negative_image(image,looping):
+    # Converts Full IMage Using Formula
+    def formula():
+        negative = 255-image
+        negative = negative.clip(0,255).astype(np.uint8)
+
+        return negative
+    # Loops Through Each Pixel And Converts Based On Formula
+    def looping_formula():
+        rows, cols = image.shape
+        negative = image.copy()
+        
+        for r in range(rows):
+            for c in range(cols):
+                negative[r][c]= 255 - image[r][c]  
+
+        return negative
+
+    negative = looping_formula() if looping else formula()
 
     return negative
 
-# Converts To A Negative Image Using LUT(look up table)
+
+
+# Negative LUT Table
+def create_lut_negative():
+    lut=[]
+    for i in range(256):
+        lut.append(255-i)
+    return lut
+
+# Converts Image To Negative Using LUT(look up table)
 def negative_image_LUT(image):
-    print()
+    lut_negative  = create_lut_negative()
+    rows, cols = image.shape
+    negative_LUT = image.copy()
+    
+    # Iterates Through Image Pixels And Converts Image Based On LUT
+    for r in range(rows):
+        for c in range(cols):
+            negative_LUT[r][c]=lut_negative[image[r][c]]
+
+    return negative_LUT
