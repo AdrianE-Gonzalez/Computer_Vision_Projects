7b8c176b3db36501435d79080e2d62f22567410c
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-02-03 14:54:14 -0600

    Orginized Test Runs

--- Convolution_and_Window_based_Operations_with_Python_OpenCV/main.py
+++ Convolution_and_Window_based_Operations_with_Python_OpenCV/main.py
@@ -4,36 +4,46 @@ import load_image
 
 import convolution
 import sobel_edge
+import median_filtering
 import noise
 import mask
 
-for x in [1]:
-    images=load_image.open_multiple_images(x)
+def run_test():
+        for x in [1]:
+        images=load_image.open_multiple_images(x)
 
-    for img in images.values():
-        
-        img_noise=noise.add_gaussian_noise(50,100,img)
-        img_s_p=noise.add_saltpepper_noise(img)
+        for img in images.values():
+                
+                img_noise=noise.add_gaussian_noise(50,100,img)
+                img_s_p=noise.add_saltpepper_noise(img)
 
-        mask_used,sum,m2,n2= convolution.create_gaussian(sigma=1)
-        mask_used2,sum2,m22,n22= convolution.create_unweighted()
-        print(mask_used2)
-        print("EMPTY SPACE")
+                # CONVOLUTION ALGORITHM
+                # Creates Masks
+                mask_1,sum_1,m2_1,n2_1= convolution.create_unweighted(mask_size=5) 
+                mask_2,sum_2,m2_2,n2_2= convolution.create_weighted(mask_size=5) 
+                mask_3,sum_3,m2_3,n2_3= convolution.create_gaussian(mask_size=5,sigma=1)
+                
+                # Applies Mask To Image
+                unweighted= mask.apply_mask(img_s_p, mask_1,sum_1,m2_1,n2_1)
+                weighted= mask.apply_mask(img_s_p, mask_2,sum_2,m2_2,n2_2)
+                gaussian= mask.apply_mask(img_s_p, mask_3,sum_3,m2_3,n2_3)
 
-        img2= mask.apply_mask(img_s_p, mask_used,sum,m2,n2)
-        img3= mask.apply_mask(img_s_p, mask_used2,sum2,m22,n22)
-        print(img)
-        print("EMPTY SPACE")
-        img4= sobel_edge.apply_sobel_edge(img)
+                #MEDIAN FILTERING
+                median= median_filtering.apply_median_filter(3,img_s_p)
 
-        
-        print(img3)
+                # SOBEL FILTERING
+                sobel= sobel_edge.apply_sobel_edge(img)
 
-        cv2.imshow("Original",img)
-        cv2.imshow("Noise Gaus",img_noise)
-        cv2.imshow("Noise SP",img_s_p)
-        cv2.imshow("Gaus",img2)        
-        cv2.imshow("Unweighted",img3) 
-        cv2.imshow("Soble Edge",img4) 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()            
+                cv2.imshow("Original",img)
+                cv2.imshow("Noise Gaus",img_noise)
+                cv2.imshow("Noise SP",img_s_p)
+                cv2.imshow("Unweighted",unweighted)        
+                cv2.imshow("Weighted",weighted) 
+                cv2.imshow("Gaussian",gaussian) 
+                cv2.imshow("Median",median) 
+                cv2.imshow("Sobel",sobel) 
+
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()            
+
+run_test()
