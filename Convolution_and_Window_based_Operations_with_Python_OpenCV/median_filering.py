90ea818158f1a7d9dbe4beaf9e38b6e1005b69b6
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-02-02 23:17:54 -0600

    Initial Commit

---
+++ Convolution_and_Window_based_Operations_with_Python_OpenCV/median_filering.py
@@ -0,0 +1,6 @@
+# To implement Median filtering 3x3 and 5x5) on a noisy image. 
+# This can be done by finding the window of pixels around the pixel to be changed, sort them, and select the median value as the replacement of that pixel. 
+# Apply the algorithm on an impulsive noise of 10% p probability of noise equally divided among salt and pepper.
+
+def apply_median_filter():
+    print()
