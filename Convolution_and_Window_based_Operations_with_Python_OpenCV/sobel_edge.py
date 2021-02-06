daa3a9c42ab846cfe4c95dee50646aaf1832805c
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-02-05 21:13:36 -0600

    Added Functions To Help Apply Different Mask Sizes Of Sobel Edge

--- Convolution_and_Window_based_Operations_with_Python_OpenCV/sobel_edge.py
+++ Convolution_and_Window_based_Operations_with_Python_OpenCV/sobel_edge.py
@@ -5,6 +5,8 @@
 import cv2
 import math
 import numpy as np
+from mask import create_mask
+from mask import apply_mask
 
 # Returns Image Filtered To Sobel Edge Based On Formula Provided Above
 def apply_sobel_edge(image):
@@ -21,3 +23,40 @@ def apply_sobel_edge(image):
             sobely= abs((-image[x-1][y-1])-(2*image[x][y-1])-(image[x+1][y-1])+(image[x-1][y+1])+(2*image[x][y+1])+(image[x+1][y+1]))
             g[x][y]=np.uint8(abs(sobelx+sobely))
     return g
+
+# Gets sobel_edge Mask Based On x    
+def apply_sobel_edge_x(mask_size):
+
+    mask,sum,m2,n2= create_mask(mask_size)
+    d=len(mask)
+
+    for x in range(-m2,m2+1):
+        for y in range(-n2,n2+1):
+            if not((x*x + y*y)==0):
+                mask[x + m2][y + n2] =  round(x / (x*x + y*y),3)
+            
+    return mask,sum,m2,n2
+
+# Gets sobel_edge Mask Based On y    
+def apply_sobel_edge_y(mask_size):
+
+    mask,sum,m2,n2= create_mask(mask_size)
+    d=len(mask)
+
+    for x in range(-m2,m2+1):
+        for y in range(-n2,n2+1):
+            if not((x*x + y*y)==0):
+                mask[x + m2][y + n2] =  round(y / (x*x + y*y),3)
+            
+    return mask,sum,m2,n2
+
+# Applies sobel_mask For x And y Combined
+# Returns Image Filtered By Sobel Edge
+def get_sobel_mask(img,mask_size):
+    mask_1,sum_1,m2_1,n2_1= apply_sobel_edge_x(mask_size=mask_size)
+    mask_2,sum_2,m2_2,n2_2= apply_sobel_edge_y(mask_size=mask_size)
+    
+    mask_=mask_1+mask_2
+    sobel= apply_mask(img, mask_,sum_1,m2_1,n2_1)
+
+    return sobel
