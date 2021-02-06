64dd23d35d5b98e71d39986f406b7d3d1a6e7106
AdrianE-Gonzalez <adrian.e.gonzalez01@gmail.com>
2021-02-05 21:14:07 -0600

    Configured Code To Run New Sobel_Edge Function

--- Convolution_and_Window_based_Operations_with_Python_OpenCV/main.py
+++ Convolution_and_Window_based_Operations_with_Python_OpenCV/main.py
@@ -21,7 +21,7 @@ def run_test(num_run):
         load_dotenv()
         SAVE_PATH = os.getenv('SAVE_PATH')
         My_Filters = SAVE_PATH+'/My_Filters'
-        OpenCV_Filters= SAVE_PATH+'/OpenCV_Filters'\
+        OpenCV_Filters= SAVE_PATH+'/OpenCV_Filters'
         
         for x in [num_run]:
                 images=load_image.open_multiple_images(x)
@@ -130,17 +130,42 @@ def run_test(num_run):
                                         # Stores openCV_median Image To Results Folder
                                         temp_openCV_filename=openCV_filename_+'/Median/openCV_Median_Filtering_'+str(matrix_)+'x'+str(matrix_)+'_'+str(count)+'.jpg'
                                         cv2.imwrite(temp_openCV_filename, openCV_median) 
-
+                                        
                                         # cv2.imshow("Unweighted Average Of "+img_n[1],unweighted)        
                                         # cv2.imshow("Weighted Average Of "+img_n[1],weighted) 
                                         # cv2.imshow("Gaussian Mask Of "+img_n[1],gaussian) 
                                         # cv2.imshow("Median Filtering Of "+img_n[1],median) 
+                        
+                        for num_ in [3,5]:
+                                if matrix_==3:
+                                        # Changes Results Path To 3x3 Matrix
+                                        filename_=My_Filters+'/Sobel_Edge/3x3_Mask/'
+                                        openCV_filename_=openCV_filename+'/Sobel_Edge/3x3_Mask/'
+
+                                if matrix_==5:
+                                        # Changes Results Path To 5x5 Matrix
+                                        filename_=filename+'/Sobel_Edge/5x5_Mask/'
+                                        openCV_filename_=OpenCV_Filters+'/Sobel_Edge/5x5_Mask/'
+
+                                # SOBEL FILTERING
+                                sobel= sobel_edge.get_sobel_mask(img,mask_size=num_)
+
+                                # Applies openCV Sobel Filter To Image
+                                openCV_sobel=openCV_mask_filters.get_sobel(img,mask_size=num_)
+
+                                # Stores sobel Image To Results Folder
+                                temp_filename=filename_+'/Sobel_Edge_'+str(count)+'.jpg'
+                                cv2.imwrite(temp_filename, sobel) 
+                                
+                                # Stores openCV_sobel Image To Results Folder
+                                temp_openCV_filename=openCV_filename_+'/openCV_Sobel_Edge_'+str(count)+'.jpg'
+                                cv2.imwrite(temp_openCV_filename, openCV_sobel)
 
                         # SOBEL FILTERING
                         sobel= sobel_edge.apply_sobel_edge(img)
 
                         # Applies openCV Sobel Filter To Image
-                        openCV_sobel=openCV_mask_filters.get_sobel(img,mask_size=5)
+                        openCV_sobel=openCV_mask_filters.get_sobel(img,mask_size=1)
 
                         # Stores sobel Image To Results Folder
                         temp_filename=My_Filters+'/Sobel_Edge/Sobel_Edge_'+str(count)+'.jpg'
@@ -167,18 +192,21 @@ def run_openCV_test(num_run):
                         openCV_unweighted= openCV_mask_filters.get_unweighted(img_s_p,mask_size=5)
                         openCV_gauss=openCV_mask_filters.get_gauss(img_s_p,mask_size=5,sigma=1)
                         openCV_median=openCV_mask_filters.get_median(img_s_p,mask_size=5)
-                        openCV_sobel=openCV_mask_filters.get_sobel(img,mask_size=5)
+                        openCV_sobel=openCV_mask_filters.get_sobel(img,mask_size=3)
+                        sobel=sobel_edge.get_sobel_mask(img,mask_size=3)
 
-                        cv2.imshow("Original",img) 
-                        cv2.imshow("Salt and Pepper Noise",img_s_p) 
-                        cv2.imshow("Gauss Noise",img_gaus) 
-                        cv2.imshow("Unwieghted",openCV_unweighted) 
-                        cv2.imshow("Gauss",openCV_gauss) 
-                        cv2.imshow("Median",openCV_median) 
-                        cv2.imshow("Sobel",openCV_sobel) 
 
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()   
+                        # cv2.imshow("Original",img) 
+                        # cv2.imshow("Salt and Pepper Noise",img_s_p) 
+                        # cv2.imshow("Gauss Noise",img_gaus) 
+                        # cv2.imshow("Unwieghted",openCV_unweighted) 
+                        # cv2.imshow("Gauss",openCV_gauss) 
+                        # cv2.imshow("Median",openCV_median) 
+                        # cv2.imshow("Sobel",openCV_sobel) 
+                        # cv2.imshow("Sobel1",sobel) 
+
+                        # cv2.waitKey(0)
+                        # cv2.destroyAllWindows()   
 
 
 # Test Runs OpenCV Filters For One Image
